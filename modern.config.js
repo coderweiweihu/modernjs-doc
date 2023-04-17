d1fd99148f1d85c608a30aa02894bfd472f6db56
huweiwei <huweiwei@keqin-it.com.cn>
2023-04-17 18:58:02 +0800

    feat: 基础知识点

--- modern.config.js
+++ modern.config.js
@@ -10,34 +10,34 @@ export default defineConfig({
           text: "介绍",
           link: "introduce",
         },
-        {
-          text: "React原理",
-          link: "library",
-        },
-        {
-          text: "基础篇",
-          link: "base",
-        },
+        // {
+        //   text: "React原理",
+        //   link: "library",
+        // },
+        // {
+        //   text: "基础篇",
+        //   link: "base",
+        // },
         {
           text: "进阶篇",
           link: "improve",
         },
-        {
-          text: "高频篇",
-          link: "high-frequency",
-        },
-        {
-          text: "精选篇",
-          link: "excellent",
-        },
-        {
-          text: "手写篇",
-          link: "handwritten",
-        },
-        {
-          text: "原理篇",
-          link: "principle",
-        },
+        // {
+        //   text: "高频篇",
+        //   link: "high-frequency",
+        // },
+        // {
+        //   text: "精选篇",
+        //   link: "excellent",
+        // },
+        // {
+        //   text: "手写篇",
+        //   link: "handwritten",
+        // },
+        // {
+        //   text: "原理篇",
+        //   link: "principle",
+        // },
         {
           text: "面经篇",
           link: "interview",
@@ -179,6 +179,26 @@ export default defineConfig({
             items: [],
           },
         ],
+        interview: [
+          {
+            text: "1 HTML模块",
+            items: [
+              {
+                text: "如何理解HTML语义化",
+                link: "/",
+              },
+            ],
+          },
+          {
+            text: "2 css模块",
+            items: [
+              {
+                text: "如何理解HTML语义化",
+                link: "/",
+              },
+            ],
+          },
+        ],
       },
       outlineTitle: "目录",
       prevPageText: "上一页",
@@ -187,7 +207,8 @@ export default defineConfig({
         message: "版权所有",
       },
     },
-    title: "前端面试资源",
+    // title: "前端面试资源",
+    title: "个人笔记",
     icon: "https://www.rspack.dev/static/image/logo.3e9ad8f8.png",
   },
   plugins: [docTools()],
